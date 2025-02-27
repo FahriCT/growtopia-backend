fd01c55a5e9a138965fd9a154800f323ca75ae8d
Senvas <84897844+FahriCT@users.noreply.github.com>
2025-02-27 19:09:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -85,6 +85,6 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
 });
